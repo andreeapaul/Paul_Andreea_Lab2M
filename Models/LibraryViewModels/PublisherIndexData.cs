6ce05e150fd6e560f44c6b60681dcce5322f14e3
ANDREEA-RAMONA PAUL <andreea.ramona.paul@stud.ubbcluj.ro>
2021-10-26 20:46:12 +0300

    Lab4

---
+++ Models/LibraryViewModels/PublisherIndexData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Paul_Andreea_Lab2.Models;
+
+namespace Paul_Andreea_Lab2.Models.LibraryViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+        public IEnumerable<Order> Orders { get; set; }
+    }
+}
